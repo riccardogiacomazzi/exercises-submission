591417f35fb831478e067f96098b1ac372066c4f
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2023-09-15 15:17:10 +0200

    commit for finished version of exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+  const [persons, setPersons] = useState([                                  // State containing the names array
+    { 
+      name: 'Arto Hellas',
+      id: 1,
+    }
   ]) 
-  const [newName, setNewName] = useState('')                                // controls Form input element
+  const [newName, setNewName] = useState('')                                // state of the inputted text to the Form
 
 
   const addNumber = (event) => {                                            // function called when the button ADD is pressed
     event.preventDefault()
-    setNewName()
     const numberObjet = {
       name: newName,
       id: persons.length +1,
     }
     setPersons(persons.concat(numberObjet))
+  }
+
+  const handleFormChange = (event) => {                                     
+    // console.log(event.target.value)
+    setNewName(event.target.value)
   }
 
   return (
@@ -23,13 +30,17 @@ const App = () => {
       <form onSubmit={addNumber}>
         <div>
           name: 
-            <input />
+            <input 
+              value={newName}
+              onChange={handleFormChange}
+            />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+          {persons.map(person => <p key={person.id}> {person.name} </p>)}
     </div>
   )
 }
