0859d1430f27f410cf7bf5b66aa527cdba4ff16a
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2023-09-18 10:55:35 +0200

    commit for phonebook step 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([                                  // State containing the names array
+  const [persons, setPersons] = useState([                                  // "Persons Array" -> State containing the names array
     { 
       name: 'Arto Hellas',
       id: 1,
@@ -9,25 +9,29 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')                                // state of the inputted text to the Form
 
-
   const addNumber = (event) => {                                            // function called when the button ADD is pressed
     event.preventDefault()
     const numberObjet = {
       name: newName,
       id: persons.length +1,
     }
-    setPersons(persons.concat(numberObjet))
+    setNewName("")
+    const allNames = persons.map(person => person.name)
+    if (allNames.includes(newName)) {                                       // If Else statement for comparing the entered text against the full array of names
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {setPersons(persons.concat(numberObjet))}
   }
 
-  const handleFormChange = (event) => {                                     
-    // console.log(event.target.value)
+
+  const handleFormChange = (event) => {                                     // handle change called when typing in the form                                 
     setNewName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
+       <form onSubmit={addNumber}>
         <div>
           name: 
             <input 
