ea9a58e0b291707c800ded6269783798b41f0a06
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2023-12-05 12:36:43 +0100

    4.1 Blog list, step1 completed

---
+++ part4/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const config = require("./utils/config");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// PASSWORD to be put into environmental variable
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
