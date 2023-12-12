3ad06654cd4ca5402daab64afc1fe52e6cfa6f47
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2023-12-12 14:53:01 +0100

    refactored requests to be async

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -5,35 +5,30 @@ const middleware = require("../utils/middleware");
 const { request, response } = require("../app");
 
 //GET
-blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouter.get("/:id", (request, response) => {
-  Blog.findById(request.params.id).then((blog) => {
-    if (blog) {
-      response.json(blog);
-    } else {
-      response.status(404).end();
-    }
-  });
+blogRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(400).end;
+  }
 });
 
 //POST
-blogRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => {
-      response.status(400).json(error.message);
-      next(error);
-    });
+blogRouter.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch (error) {
+    response.status(400).json(error.message);
+    next(error);
+  }
 });
 
 //DELETE
