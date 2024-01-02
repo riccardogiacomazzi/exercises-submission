5880c5783dd8acef1fb3c3fbdbb18370cac7bd5d
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2024-01-02 16:35:24 +0100

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "../components/Blog";
+import Togglable from "./Togglable";
 
 const userLogged = {
   username: "admin",
@@ -9,29 +11,46 @@ const userLogged = {
   id: "6581b11dad414436adf04a38",
 };
 
-test("Title and Author are correctly rendered", () => {
-  jest.mock("./RemoveButton", () => ({
-    __esModule: true,
-    default: jest.fn(() => null),
-  }));
-
-  const blogs = [
-    {
-      title: "Test blog title is rendered",
-      author: "Tester author",
-      url: "www.test.com",
-      likes: 0,
-      user: {
-        username: "admin",
-        name: "admino",
-        id: "6581b11dad414436adf04a38",
-      },
-      id: "6585c7fbd17394973f7a0485",
+const blogs = [
+  {
+    title: "Test blog title is rendered",
+    author: "Tester author",
+    url: "www.test.com",
+    likes: 0,
+    user: {
+      username: "admin",
+      name: "admino",
+      id: "6581b11dad414436adf04a38",
     },
-  ];
+    id: "6585c7fbd17394973f7a0485",
+  },
+];
 
-  const { container } = render(<Blog blogs={blogs} user={userLogged} />);
+describe("<Blog />:", () => {
+  test("Title and Author are correctly rendered", () => {
+    const { container } = render(<Blog blogs={blogs} user={userLogged} />);
 
-  const div = container.querySelector(".blogTitle");
-  expect(div).toHaveTextContent("Test blog title is rendered by Tester author");
+    const div = container.querySelector(".blogTitle");
+    expect(div).toHaveTextContent("Test blog title is rendered by Tester author");
+  });
+});
+
+describe("<Togglable />:", () => {
+  let container;
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel="info">
+        <div className="testDiv">togglable content is shown</div>
+      </Togglable>
+    ).container;
+  });
+  test("URL and Likes are shown when the info button has been clicked", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("info");
+    await user.click(button);
+
+    const div = container.querySelector(".togglableContent");
+    expect(div).toHaveTextContent("togglable content is shown");
+  });
 });
