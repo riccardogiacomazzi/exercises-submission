359c41f1afe6c525b7eed29753c8d2c96929d1b7
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2024-01-03 11:06:23 +0100

    5.15: Blog list tests, step3 - completed

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -26,6 +26,8 @@ const blogs = [
   },
 ];
 
+const mockLike = jest.fn();
+
 describe("<Blog />:", () => {
   test("Title and Author are correctly rendered", () => {
     const { container } = render(<Blog blogs={blogs} user={userLogged} />);
@@ -41,16 +43,41 @@ describe("<Togglable />:", () => {
   beforeEach(() => {
     container = render(
       <Togglable buttonLabel="info">
-        <div className="testDiv">togglable content is shown</div>
+        <div className="testDiv">
+          <p>togglable content is shown</p>
+        </div>
       </Togglable>
     ).container;
   });
+
   test("URL and Likes are shown when the info button has been clicked", async () => {
+    const blog = blog;
+
     const user = userEvent.setup();
     const button = screen.getByText("info");
     await user.click(button);
 
     const div = container.querySelector(".togglableContent");
     expect(div).toHaveTextContent("togglable content is shown");
+  });
+
+  test("If Like is pressed twice, it is received twice", async () => {
+    const blog = blogs;
+
+    render(
+      <Togglable buttonLabel="info" buttonLabelClose="hide">
+        <ul>{blog.url}</ul>
+        <ul>
+          Likes: {blog.likes} <button onClick={() => mockLike(blog.id)}>like</button>
+        </ul>
+      </Togglable>
+    );
+
+    const user = userEvent.setup();
+    const button = screen.getByText("like");
+    await user.click(button);
+    await user.click(button);
+
+    expect(mockLike.mock.calls).toHaveLength(2);
   });
 });
