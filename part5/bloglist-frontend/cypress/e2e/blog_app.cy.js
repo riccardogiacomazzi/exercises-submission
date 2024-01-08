8a3e4335fc2cb559b9f63ad2bad2300705588699
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2024-01-08 15:29:37 +0100

    5.17: bloglist end to end testing, step1 - cmopleted

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,16 @@
 describe("Blog app", () => {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const initUser = {
+      username: "riccar_dog",
+      name: "Riccardo G",
+      password: "password",
+    };
+    cy.request("POST", "http://localhost:3001/api/users", initUser);
+    cy.visit("http://localhost:5173/");
   });
 
-  it("passes", () => {});
+  it("displays Login Form as default", function () {
+    cy.contains("Log in to Blog App");
+  });
 });
