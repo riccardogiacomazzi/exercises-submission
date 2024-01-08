833e69f794f027681c30d3c6e7f3413c8029ac81
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2024-01-08 15:48:40 +0100

    5.18: bloglist end to end testing, step2 - completed

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,4 @@
-describe("Blog app", () => {
+describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
     const initUser = {
@@ -12,5 +12,20 @@ describe("Blog app", () => {
 
   it("displays Login Form as default", function () {
     cy.contains("Log in to Blog App");
+  });
+
+  describe("Login", function () {
+    it("Succesfully works with correct credentials", function () {
+      cy.get("#username").type("riccar_dog");
+      cy.get("#password").type("password");
+      cy.get("#login-button").click();
+      cy.contains("Logged in as");
+    });
+    it("Doesn't work with wrong credentials", function () {
+      cy.get("#username").type("wrong_username");
+      cy.get("#password").type("wrong_password");
+      cy.get("#login-button").click();
+      cy.contains("Log in to Blog App");
+    });
   });
 });
